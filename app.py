34ae55159b8497dcd5270759c8c2d6eae70fe126
0xrushi <0xrushi@gmail.com>
2024-08-26 00:53:46 -0400

    restore app.run

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def add_linebreaks(p, line_length):
 
 if __name__ == "__main__":
     with app.app_context():
-        app.run(debug=True, host='0.0.0.0', port=5000) 
+        app.run(debug=True, port=5000) 
